0043c351f6942d0175dd986fc616dfd05ba57ea3
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-05-11 18:16:42 +0300

    task 2: user entity

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,32 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+let users = [
+  {
+    id: '1',
+    name: 'string',
+    login: 'string2',
+    password: 'string3',
+  },
+];
+
+const getAll = async () => users;
+
+const create = async (user) => {
+  users = [...users, user];
+  return user;
 };
 
-module.exports = { getAll };
+const getById = async (id) => users.find((user) => user.id === id);
+
+const update = async (userToUpdate) => {
+  const filteredUsers = users.filter((user) => user.id !== userToUpdate.id);
+  users = [...filteredUsers, userToUpdate];
+  return userToUpdate;
+};
+
+const remove = async (id) => {
+  const filteredUsers = users.filter((user) => user.id !== id);
+  const userToDelete = users.find((user) => user.id === id);
+  users = [...filteredUsers];
+  return userToDelete;
+};
+
+module.exports = { getAll, create, getById, update, remove };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,20 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const create = (data) => {
+  const createdUser = new User({ ...data, id: undefined });
+  return usersRepo.create(createdUser);
+};
+
+const getById = (id) => usersRepo.getById(id);
+
+const update = (id, data) => {
+  const user = new User({ ...data, id });
+  return usersRepo.update(user);
+};
+
+const remove = (id) => usersRepo.remove(id);
+
+module.exports = { getAll, create, getById, update, remove };
