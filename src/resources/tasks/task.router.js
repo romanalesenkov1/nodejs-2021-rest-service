675a8f76eba85f1ac4ebd6fb9b6162a2e842e6e9
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-05-13 10:13:00 +0300

    task 2: refactor task router

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 const tasksService = require('./task.service');
 
-router.route('/:boardId/tasks/').get(async (req, res) => {
+router.route('/').get(async (req, res) => {
   const tasks = await tasksService.getAllByBoardId(req.params.boardId);
   res.json(tasks);
 });
 
-router.route('/:boardId/tasks/').post(async (req, res) => {
+router.route('/').post(async (req, res) => {
   const task = await tasksService.create(req.body, {
     boardId: req.params.boardId,
   });
   res.status(201).json(task);
 });
 
-router.route('/:boardId/tasks/:taskId').get(async (req, res) => {
+router.route('/:taskId').get(async (req, res) => {
   const task = await tasksService.getByBoardIdByTaskId(
     req.params.boardId,
     req.params.taskId
@@ -25,7 +25,7 @@ router.route('/:boardId/tasks/:taskId').get(async (req, res) => {
   }
 });
 
-router.route('/:boardId/tasks/:taskId').put(async (req, res) => {
+router.route('/:taskId').put(async (req, res) => {
   const task = await tasksService.update({
     ...req.body,
     boardId: req.params.boardId,
@@ -39,7 +39,7 @@ router.route('/:boardId/tasks/:taskId').put(async (req, res) => {
   }
 });
 
-router.route('/:boardId/tasks/:taskId').delete(async (req, res) => {
+router.route('/:taskId').delete(async (req, res) => {
   const task = await tasksService.remove(req.params.taskId);
   if (!task) {
     res.status(404).send('Task not found');
