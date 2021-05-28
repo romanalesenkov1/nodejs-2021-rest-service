5a756c3e50bf6f156e5b63a7602c43d30b7efe30
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-05-28 19:45:50 +0300

    Task 4: configure build + ts + eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { PORT } from './common/config.js';
-import app from './app.ts';
+import { PORT } from './common/config';
+import app from './app';
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
