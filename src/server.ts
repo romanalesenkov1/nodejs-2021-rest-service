4494d120697366c8af43252bfa41d1a572a5fb44
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-05-28 03:09:22 +0300

    Task 4: add TypeScript

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { PORT } from './common/config.js';
-import app from './app.js';
+import app from './app.ts';
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
