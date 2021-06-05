ce97e16aaf53c1991ec229e5a730b3d07042c724
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-06-06 02:28:44 +0400

    Task 4: add winston

---
+++ src/middleware/logger/requests.logger.ts
@@ -0,0 +1,34 @@
+import winston from 'winston';
+import expressWinston from 'express-winston';
+
+export const requestsLogger = expressWinston.logger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error',
+            format: winston.format.combine(
+                winston.format.uncolorize(),
+                winston.format.json()
+            )
+        }),
+        new winston.transports.File({
+            filename: 'info.log',
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.uncolorize(),
+                winston.format.json()
+            )
+        }),
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    ),
+    meta: false, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}} | query parameters: {{JSON.stringify(req.query)}} | body: {{JSON.stringify(req.body)}} | status code: {{res.statusCode}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute () { return false; } // optional: allows to skip some log messages based on request and/or response
+});
