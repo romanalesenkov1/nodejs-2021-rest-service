738f7e511c4d06e990a4402d24bcc53d1aea248d
Roman Alesenkov <raman.alesiankou@softswiss.com>
2021-06-08 13:56:21 +0400

    Task 5: refactor build

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,9 +1,5 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config({
   path: path.join(__dirname, '../../.env'),
